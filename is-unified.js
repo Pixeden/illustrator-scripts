7757806943a15ed1492e6ca830a05777d4e8927c
Lionel T <elrumordelaluz@hotmail.com>
2017-03-07 11:13:06 +0100

    Add is-unified script

---
+++ is-unified.js
@@ -0,0 +1,71 @@
+#target Illustrator
+
+//  script.name = is-unified.js;  
+//  script.description = Check if all paths in each Artboard has unified style;  
+//  script.requirement = one document with at least one artboard;  
+//  script.parent = elrumordelaluz;  
+//  script.elegant = false; 
+
+
+if (app.documents.length > 0) {
+  var doc = app.activeDocument
+  checkUnifiedStyle()
+} else {
+  alert ("There is no open document.")
+}
+
+function checkUnifiedStyle() {
+  var pathCount = doc.pathItems.length
+  var compPathCount = doc.compoundPathItems.length
+  
+  for (i = 0; i < doc.artboards.length; i++) {
+    selectArtboardByIndex(i)
+    var styles = new Array()
+    
+    // Simple Paths Loop
+    if (pathCount > 0) {
+      for (var y = 0; y < pathCount; y++) {
+        if (doc.pathItems[y].selected) {
+          styles = styles.concat(getStyle(doc.pathItems[y]))
+        }
+      }
+    }    
+    
+    if (styles.length > 1) {
+      if (!hasSameValues(styles)) {
+        return alert('The Artboard: "' + doc.artboards[i].name + '" have mixed styles.')
+      }
+    }
+    
+  }
+  
+  return alert('Everithing ok!')
+}
+
+function hasSameValues (arr) {
+  var acc = arr[0]
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] !== acc) {
+      return false
+    }
+  }
+  return true
+}
+
+function isStroked (path) { return path.stroked && !path.filled }
+function isFilled (path) { return path.filled && !path.stroked && path.opacity === 100 }
+function getStyle (path) {
+  if (isStroked(path)) {
+    return 'stroked'
+  } else if (isFilled(path)) {
+    return 'filled'
+  } else {
+    return 'mixed'
+  }
+}
+
+function selectArtboardByIndex (index) {
+  doc.selection = null
+  doc.artboards.setActiveArtboardIndex(index)
+  doc.selectObjectsOnActiveArtboard()
+}
