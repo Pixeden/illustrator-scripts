9c5ddfb282ea0a61087cc6c8d84fbead9dd1308c
Lionel T <elrumordelaluz@hotmail.com>
2017-08-07 15:27:16 +0200

    Add export-png script

---
+++ export-png.js
@@ -0,0 +1,56 @@
+#target Illustrator
+
+//  script.name = export-png.js;
+//  script.description = Exports Artboards into separated .png files;
+//  script.requirement = one document with at least one artboard;
+//  script.parent = elrumordelaluz;
+//  script.elegant = false;
+
+if (app.documents.length > 0) {
+  var doc = app.activeDocument
+  exportPNG()
+} else {
+  alert('There is no open document.')
+}
+
+function exportPNG() {
+  var packName = Window.prompt ("Enter the Prefix String", 'Base Name')
+  
+  if (packName.replace(/\s/g, '').length) {
+    var exportOptions = new ExportOptionsPNG24()
+    exportOptions.artBoardClipping = true
+    exportOptions.antiAliasing = false
+    exportOptions.transparency = false
+    
+    var type = ExportType.PNG24
+    
+    var folder1x = new Folder(doc.path + '/1x')
+    var folder2x = new Folder(doc.path + '/2x')
+    var folder3x = new Folder(doc.path + '/3x')
+    if(!folder1x.exists) folder1x.create();
+    if(!folder2x.exists) folder2x.create();
+    if(!folder3x.exists) folder3x.create();
+    
+    for (i = 0; i < doc.artboards.length; i++) {
+      var starting_artboard = doc.artboards.setActiveArtboardIndex(i);
+      var artboardName = doc.artboards[i].name
+      var destFile;
+      
+      exportOptions.horizontalScale = exportOptions.verticalScale = 100
+      var destFile = new File(folder1x + '/' + packName + '_' + artboardName +  '.png' );  
+      doc.exportFile(destFile, type, exportOptions)
+      
+      exportOptions.horizontalScale = exportOptions.verticalScale = 200
+      var destFile = new File(folder2x + '/' + packName + '_' + artboardName +  '.png' );  
+      doc.exportFile(destFile, type, exportOptions)
+      
+      exportOptions.horizontalScale = exportOptions.verticalScale = 300
+      var destFile = new File(folder3x + '/' + packName + '_' + artboardName +  '.png' );  
+      doc.exportFile(destFile, type, exportOptions)
+    }
+  } else {
+    alert('Please insert a value')
+    exportPNG()
+  }
+  
+}
